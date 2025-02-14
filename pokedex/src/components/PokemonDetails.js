bda2c9c25eb2862a7570bee9754bd951bf7dbbd4
toxophil <theophile.vaudour@yahoo.fr>
2025-02-14 15:33:08 +0100

    feat: ajout de l'image de l'évolution du pokemon

--- pokedex/src/components/PokemonDetails.js
+++ pokedex/src/components/PokemonDetails.js
@@ -18,9 +18,9 @@ function PokemonDetails({ pokemon, onClose, backgroundStyle }) {
       textAlign: "center",
       borderRadius: "10px",
       padding: "20px",
-      background: backgroundStyle.background, // Appliquer le même dégradé !
+      background: backgroundStyle.background,
       boxShadow: "0px 4px 10px rgba(0, 0, 0, 0.3)",
-      color: "#fff", // Texte blanc pour un meilleur contraste
+      color: "#fff",
     },
   };
 
@@ -33,33 +33,46 @@ function PokemonDetails({ pokemon, onClose, backgroundStyle }) {
       contentLabel="Détails du Pokémon"
     >
       <h2>{pokemon.Name}</h2>
+      
+      {/* Image du Pokémon actuel */}
       <img 
         src={`/images/${pokemon.Name.toLowerCase()}.png`} 
         alt={pokemon.Name} 
-        style={{ width: "100px", height: "100px", objectFit: "contain" }} 
+        style={{ width: "120px", height: "120px", objectFit: "contain" }} 
       />
+
       <p>Type: {pokemon.Type1} {pokemon.Type2 && `/ ${pokemon.Type2}`}</p>
 
       {/* Icônes des types */}
       <div className="pokemon-types-modal">
-  {icons[pokemon.Type1] && (
-    <div
-      className="type-icon-modal pulsing glow"
-      style={{ "--type-color": `#${icons[pokemon.Type1]?.color || "ccc"}` }}
-      dangerouslySetInnerHTML={{ __html: icons[pokemon.Type1].svg }}
-    />
-  )}
-  {pokemon.Type2 && icons[pokemon.Type2] && (
-    <div
-      className="type-icon-modal pulsing glow"
-      style={{ "--type-color": `#${icons[pokemon.Type2]?.color || "ccc"}` }}
-      dangerouslySetInnerHTML={{ __html: icons[pokemon.Type2].svg }}
-    />
-  )}
-</div>
+        {icons[pokemon.Type1] && (
+          <div
+            className="type-icon-modal pulsing glow"
+            style={{ "--type-color": `#${icons[pokemon.Type1]?.color || "ccc"}` }}
+            dangerouslySetInnerHTML={{ __html: icons[pokemon.Type1].svg }}
+          />
+        )}
+        {pokemon.Type2 && icons[pokemon.Type2] && (
+          <div
+            className="type-icon-modal pulsing glow"
+            style={{ "--type-color": `#${icons[pokemon.Type2]?.color || "ccc"}` }}
+            dangerouslySetInnerHTML={{ __html: icons[pokemon.Type2].svg }}
+          />
+        )}
+      </div>
 
-
-      {pokemon.Evolution && <p>Évolution: {pokemon.Evolution}</p>}
+      {/* Affichage de l'évolution avec son image */}
+      {pokemon.Evolution && (
+        <div className="evolution-section">
+          <h3>Évolution</h3>
+          <p>{pokemon.Evolution}</p>
+          <img 
+            src={`/images/${pokemon.Evolution.toLowerCase()}.png`} 
+            alt={pokemon.Evolution} 
+            style={{ width: "100px", height: "100px", objectFit: "contain" }} 
+          />
+        </div>
+      )}
 
       <button className="close-button" onClick={onClose}>
         Fermer
